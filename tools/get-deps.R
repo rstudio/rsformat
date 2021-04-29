b8f8216cae6ed64c8feefaa6a91ddcd5e5a8fcd5
christophe dervieux <christophe.dervieux@gmail.com>
2021-04-29 22:42:39 +0200

    Add bslib bs3compat sass layer to the bundle
    Currently done by adding correct layers in the rstrap scss file.
    This fixes issue with styling in html_document().
    It was mainly some compatibility issue with BS3 regarding the html_document() feature and template;

--- tools/get-deps.R
+++ tools/get-deps.R
@@ -7,3 +7,15 @@ pak::pak("fs")
 
 bs3comptat_dir <- system.file("bs3compat", package = "bslib")
 fs::dir_copy(bs3comptat_dir, "tools/rstrap/")
+
+## Adapt scss for our bundle
+withr::with_dir(
+  "tools/rstrap/bs3compat/", {
+    ## rename file to avoid conflict
+    files <- c("_declarations", "_defaults", "_rules")
+    new <- paste0(files, "_bs3_compat")
+    try(fs::file_move(fs::path_ext_set(files, ".scss"), fs::path_ext_set(new, ".scss")))
+    ## Remove rules we don't need
+    xfun::gsub_file("_rules_bs3_compat.scss", "^(@import \"wells\";)$", "// \\1")
+})
+
