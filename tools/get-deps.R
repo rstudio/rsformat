6e3696972ed4e6c455d46db5a08afc93da08a17c
christophe dervieux <christophe.dervieux@gmail.com>
2021-05-12 18:21:18 +0200

    remove glyphicons from b3compat and we don't want to include the fonts for now
    closes #10

--- tools/get-deps.R
+++ tools/get-deps.R
@@ -6,7 +6,9 @@ pak::pak("fs")
 # Get bs3 compat ----------------------------------------------------------
 
 bs3comptat_dir <- system.file("bs3compat", package = "bslib")
-fs::dir_copy(bs3comptat_dir, "tools/rstrap/")
+where <- fs::path("tools/rstrap/", fs::path_file(bs3comptat_dir))
+fs::dir_delete(where)
+fs::dir_copy(bs3comptat_dir, where )
 
 ## Adapt scss for our bundle
 withr::with_dir(
@@ -17,5 +19,9 @@ withr::with_dir(
     try(fs::file_move(fs::path_ext_set(files, ".scss"), fs::path_ext_set(new, ".scss")))
     ## Remove rules we don't need
     xfun::gsub_file("_rules_bs3_compat.scss", "^(@import \"wells\";)$", "// \\1")
+    xfun::gsub_file("_rules_bs3_compat.scss", "^(@import \"glyphicons\";)$", "// \\1")
 })
 
+# Build rstrap
+
+# See tools/update.sh
